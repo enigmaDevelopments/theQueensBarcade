0e6657032c1ca6b767a72800eb380b7dd9ac549d
enigmaDevelopments <enigmarezac@gmail.com>
2025-01-06 14:50:41 -0500

    make game class

--- game.py
+++ game.py
@@ -1,95 +1,67 @@
 import tkinter as tk
 from tkinter import messagebox
-import random
 import subprocess
 import simplejson
+from gameState import gameState
 
+class game :
+  def __init__(self, botOn): 
+    self.window = tk.Tk()
+    self.window.title("Queens barcade")
+    self.window.geometry("152x152")
+    self.peice = (tk.PhotoImage(file="queen p1.png"),tk.PhotoImage(file="queen p2.png"))
+    self.white = tk.PhotoImage(file="white.png")
+    self.wall = tk.PhotoImage(file="wall.png")
+    self.state = gameState((set(),(13,14,0,3),(),True))
+    self.botOn = botOn
+    self.space = []
+    self.again = None
+    for i in range (16):
+      self.space.append(tk.Button(self.window,image=self.white,height=32,width=32,bd=2,bg="black",relief='flat',highlightthickness=0,command=lambda num=i :self.click(num)))
+      self.space[i].grid(row=i//4,column=i%4)
+    for x in [13,14,0,3] :
+      self.space[x].config(image = self.peice[int(((x+6)**-1)*10)])
+    tk.mainloop()
+    if self.again == "yes":
+      game(botOn)
 
-
-window = tk.Tk()
-window.title("Queens barcade")
-window.geometry("152x152")
-
-botOn = True
-with open('botOn.jason') as bot:
-  botOn = simplejson.load(bot)
-
-
-ai = {}
-moves = []
-defultProbMap = []
-if botOn :
-  with open('ai.jason') as aiData:
-    ai = simplejson.load(aiData)
-  for num in range(16) :
-    defultProbMap.append([.1,.1])
-
-
-peice = [tk.PhotoImage(file="queen p1.png"),tk.PhotoImage(file="queen p2.png")]
-white = tk.PhotoImage(file="white.png")
-wall = tk.PhotoImage(file="wall.png")
-wallLocations = ""
-peiceLoactions = "de03"
-peiceSelected = -1
-p1Turn = True
-winCon = ''
-again = "no"
-
-def bot() :
-  global ai
-  global moves
-  map = ("".join(sorted(peiceLoactions[:2])) + "".join(sorted(peiceLoactions[2:])) + "".join(sorted(wallLocations)))
-  rand = random.randint(0,15)
-  while random.randint(0,ai.get(map,defultProbMap)[rand][(peiceSelected+4)//4]*100//1) :
-    rand = random.randint(0,15)
-  moves.append([map,rand,(peiceSelected+4)//4])
-  return rand
-
-def inLoc(loc) :
-  for i in range(4) :
-      if hex(loc)[-1] == peiceLoactions[i] :
-        return i
-  return -1
-def surounded(loc,isPlayer1) :
-  if loc == "g" :
+  def click(self,loc):
+    newState = self.state.move((loc,))
+    if newState == None :
+      return True
+    elif newState == 0:
+      self.space[loc].config(bg="green")
+      return True
+    elif newState == 1:
+      self.space[loc].config(bg="black")
+      return True
+    if (self.state.peiceLoactions == newState[1]):
+      self.space[loc].config(image = self.wall)
+    else:
+      self.space[self.state.peiceSelected].config(image=self.white,bg="black")
+      self.space[loc].config(image = self.peice[0 if self.state.p1Turn else 1])
+    self.state = gameState(newState)
+    end = self.state.endingBoard()
+    winCon = None
+    if end == 1:
+      winCon = "P1 win"
+    elif end == 2:
+      winCon = "Tie"
+    elif end == 3:
+      winCon = "P2 win"
+    if end != 0:
+      messagebox.showinfo('Game end', winCon)
+      self.again = messagebox.askquestion('Game end', 'Play again?')
+      self.window.destroy()
     return False
-  loc = int(loc,base=16)
-  surrounding = []
-  if int(loc / 4) > 0:
-    surrounding.append(loc - 4)
-    if loc % 4 > 0:
-      surrounding.append(loc - 5)
-    if loc % 4 < 3:
-      surrounding.append(loc - 3)
-  if int(loc / 4) < 3:
-    surrounding.append(loc + 4)
-    if loc % 4 > 0:
-      surrounding.append(loc + 3)
-    if loc % 4 < 3:
-      surrounding.append(loc + 5)
-  if loc % 4 > 0:
-    surrounding.append(loc - 1)
-  if loc % 4 < 3:
-    surrounding.append(loc + 1)
-  for i in range(len(surrounding)) :
-    if not(hex(surrounding[i])[-1] in wallLocations or (hex(surrounding[i])[-1] in peiceLoactions[:2] and isPlayer1) or (hex(surrounding[i])[-1] in peiceLoactions[2:] and not isPlayer1)): 
-      return False
-  return True
+    
 
-def inLine(loc) :
-  if peiceSelected == -1 or int(peiceLoactions[peiceSelected],base=16) == loc:
-    return True
-  toAdd = [-5,-4,-3,-1,1,3,4,5]
-  for j in range(8) :
-    i = int(peiceLoactions[peiceSelected],base=16) + toAdd[j]
-    cont = True
-    while (i >= 0 and i < 16) and cont:
-      if hex(i)[-1] in wallLocations or ((p1Turn and hex(i)[-1] in peiceLoactions[:2]) or (not p1Turn and hex(i)[-1] in peiceLoactions[2:])) or (hex(i)[-1] in peiceLoactions and i !=loc) or (((i-toAdd[j])%4==0 and (i)%4==3) or ((i-toAdd[j])%4==3 and (i)%4==0)) :
-        cont = False
-      elif i == loc :
-        return True
-      i+=toAdd[j]
-  return False
+      
+
+      
+    
+    
+
 
 def click(loc) :
   global peiceSelected
@@ -150,23 +122,5 @@ def click(loc) :
     moves.pop()
   return False
 
-space = []
-for i in range (16):
-  space.append( tk.Button(window,image=white,height=32,width=32,bd=2,bg="black",relief='flat',highlightthickness=0,command=lambda num=i :click(num)))
-  space[i].grid(row=i//4,column=i%4)
-for x in [13,14,0,3] :
-  space[x].config(image = peice[int(((x+6)**-1)*10)])
-tk.mainloop()
 
-winCon = False if winCon == 'tie' else (.5 if winCon == "P2 win" else 2)
-if botOn and winCon :
-  for game in moves :
-    probMap = ai.get(game[0],defultProbMap.copy())
-    probMap[game[1]][game[2]]*=winCon
-    ai.setdefault(game[0],probMap)
-  with open('ai.jason','w') as aiData:
-    simplejson.dump(ai,aiData)
-if again == 'yes' :
-  subprocess.run(["python", "game.py"])
-else :
-  subprocess.run(["python", "main.py"])
+test = game(False)
--- gameState.py
+++ gameState.py
@@ -7,6 +7,8 @@ class gameState:
         self.peiceSelected = -1
 
     def surounded(self,loc,isPlayer1) :
+        if loc == 16:
+            return True
         surrounding = set()
         if loc // 4 > 0:
             surrounding.add(loc - 4)
@@ -28,9 +30,11 @@ class gameState:
 
     def vaildMoves(self) :
         moves = list(set(range(16)) - self.wallLocations-set(self.peiceLoactions))
-        peices = self.peiceLoactions[2:] if self.p1Turn else self.peiceLoactions[:2]
+        peices = self.peiceLoactions[:2] if self.p1Turn else self.peiceLoactions[2:]
         toAdd = (-5,-4,-3,-1,1,3,4,5)
         for i in peices:
+            if i == 16:
+                continue  
             move = [i]
             for j in toAdd:
                 h = i + j
@@ -39,7 +43,7 @@ class gameState:
                 while (h >= 0 and h < 16) and cont:
                     if h in self.wallLocations or (((h-j)%4==0 and h%4==3) or ((h-j)%4==3 and (h)%4==0)) or not cont2 :
                         cont = False
-                    elif (self.p1Turn and h in self.peiceLoactions[:2]) or (not self.p1Turn and h in self.peiceLoactions[2:]) :
+                    elif (self.p1Turn and h in self.peiceLoactions[2:]) or (not self.p1Turn and h in self.peiceLoactions[:2]) :
                         cont2 = False
                         move.append(h)
                     elif h in self.peiceLoactions :
@@ -47,7 +51,7 @@ class gameState:
                     else:
                         move.append(h)
                     h+=j
-                moves.append(tuple(move))
+            moves.append(tuple(move))
         return tuple(moves)
     
     def endingBoard (self) :
@@ -55,29 +59,39 @@ class gameState:
         for i in range (4) :
             if self.surounded(self.peiceLoactions[i],not i//2):
                 peicesGone[i//2] += 1
+        print(peicesGone)
         if peicesGone[0] == 2 and peicesGone[1] == 2 :
             return 2
         elif peicesGone[0] == 2 :
-            return 1
-        elif  peicesGone[1] == 2 :
             return 3
+        elif  peicesGone[1] == 2 :
+            return 1
         elif self.prevStates.count(self.peiceLoactions) == 2 :
             return 2
         return 0
     
     def move(self,loc):
         vaild = self.vaildMoves()
-        if loc[0] in vaild:
-            return (self.wallLocations | {loc[0]}, tuple(self.peiceLoactions), (), not self.p1Turn)
-        peices = list(self.peiceLoactions)
         for i in vaild :
             if type(i) == tuple :
-                if self.peiceSelected == i[0] and loc[0] in i[:1]:
-                    peices[peices.index(i[0])]  = loc[0] 
-                    return (self.wallLocations, tuple(peices),self.prevStates + (self.peiceLoactions,), not self.p1Turn)
+                if self.peiceSelected == i[0] and loc[0] in i[1:] and loc[0] != self.peiceSelected:
+                    return self.peiceMove(loc[0])
                 elif self.peiceSelected == -1 and loc[0] == i[0] :
                     if len(loc) == 2 and loc[1] in i:
-                        peices[peices.index(i[0])]  = loc[1]
-                        return (self.wallLocations, tuple(peices), self.prevStates + (self.peiceLoactions,), not self.p1Turn)
+                        self.peiceSelected = loc[0]
+                        return self.peiceMove(loc[1])
                     else:
                         self.peiceSelected = i[0]
+                        return 0
+        if loc[0] in vaild:
+            return (self.wallLocations | {loc[0]}, tuple(self.peiceLoactions), (), not self.p1Turn)
+        if self.peiceSelected == loc[0]:
+            self.peiceSelected = -1
+            return 1
+    
+    def peiceMove(self, loc):
+        peices = list(self.peiceLoactions)
+        if loc in peices:
+            peices[peices.index(loc)]  = 16
+        peices[peices.index(self.peiceSelected)]  = loc
+        return (self.wallLocations, tuple(peices), self.prevStates + (self.peiceLoactions,), not self.p1Turn)
