1d2ea7a146f3a1187ca723552ce86d9e659b2443
enigmaDevelopments <enigmarezac@gmail.com>
2025-02-04 11:40:26 -0500

    separated movement matrixes

--- cubot.py
+++ cubot.py
@@ -18,16 +18,19 @@ w = cp.array(((0,1,1,1),(1,1,1,1),(1,0,1,1),(1,1,1,1)))
 positions = cp.array((p,p))
 player = cp.where(positions < 5 ,positions,0)
 walls = cp.array(((cp.array((w,w)) * cp.where(((5 < positions) | (positions == 0)),1,0)), cp.array((w,w)) * cp.where(positions == 0,1,0)))
-moves = walls[1].copy()
+moves = cp.zeros_like(positions)
 
 for i in cp.arange(len(directions)):
-    block = walls * edges[i]
-
     pos = player.copy()
+    block = walls * edges[i]
 
     for j in cp.arange(3):
         pos = cp.roll(pos,directions[i])
         moves += pos * block[0]
         pos *= block[1]
+p1Moves = moves & 2
+p2Moves = moves & 4
 
-print(moves)
+print(walls[1])
+print(p1Moves)
+print(p2Moves)
