e380a5587d0960c35ed772f86bc80903ff36edd3
enigmaDevelopments <enigmarezac@gmail.com>
2025-02-04 15:44:48 -0500

    made getMoves compatible with boolean matrices

--- cubot.py
+++ cubot.py
@@ -1,45 +1,45 @@
 import cupy as cp
 
 edges = cp.array((
-    ((1,1,1,0),
-     (1,1,1,0),
-     (1,1,1,0),
-     (0,0,0,0)),
+    ((True,True,True,False),
+     (True,True,True,False),
+     (True,True,True,False),
+     (False,False,False,False)),
 
-    ((1,1,1,1),
-     (1,1,1,1),
-     (1,1,1,1),
-     (0,0,0,0)),
+    ((True,True,True,True),
+     (True,True,True,True),
+     (True,True,True,True),
+     (False,False,False,False)),
 
-    ((0,1,1,1),
-     (0,1,1,1),
-     (0,1,1,1),
-     (0,0,0,0)),
+    ((False,True,True,True),
+     (False,True,True,True),
+     (False,True,True,True),
+     (False,False,False,False)),
 
-    ((1,1,1,0),
-     (1,1,1,0),
-     (1,1,1,0),
-     (1,1,1,0)),
+    ((True,True,True,False),
+     (True,True,True,False),
+     (True,True,True,False),
+     (True,True,True,False)),
      
-    ((0,1,1,1),
-     (0,1,1,1),
-     (0,1,1,1),
-     (0,1,1,1)),
+    ((False,True,True,True),
+     (False,True,True,True),
+     (False,True,True,True),
+     (False,True,True,True)),
 
-    ((0,0,0,0),
-     (1,1,1,0),
-     (1,1,1,0),
-     (1,1,1,0)),
+    ((False,False,False,False),
+     (True,True,True,False),
+     (True,True,True,False),
+     (True,True,True,False)),
 
-    ((0,0,0,0),
-     (1,1,1,1),
-     (1,1,1,1),
-     (1,1,1,1)),
+    ((False,False,False,False),
+     (True,True,True,True),
+     (True,True,True,True),
+     (True,True,True,True)),
 
-    ((0,0,0,0),
-     (0,1,1,1),
-     (0,1,1,1),
-     (0,1,1,1)),
+    ((False,False,False,False),
+     (False,True,True,True),
+     (False,True,True,True),
+     (False,True,True,True)),
 ))
 
 surrounding = cp.array((
@@ -119,7 +119,7 @@ surrounding = cp.array((
     ((0,0,0,0),
      (0,0,0,0),
      (0,1,1,1),
-     (0,1,-.5,1))
+     (0,1,-.5,1)),
 
     ((0,0,0,0),
      (0,0,0,0),
@@ -132,40 +132,42 @@ directions = cp.array((-5,-4,-3,-1,1,3,4,5))
 def score(walls: cp.ndarray, pieces: cp.ndarray):
     return 0
 
-def getMoves(walls: cp.ndarray, positions: cp.ndarray, p1Turn: bool):
+def getMoves(walls: cp.ndarray, p1: cp.ndarray,p2: cp.ndarray,p3: cp.ndarray,p4: cp.ndarray, p1Turn: bool):
     global edges
     global directions
 
     if p1Turn:
-        player = cp.where(positions < 5 ,positions,0)
-        wall = cp.array(((walls * cp.where(((5 < positions) | (positions == 0)),1,0)), walls * cp.where(positions == 0,1,0)))
+        player = (p1 * 2) + (p2 * 4)
+        wall = cp.array((walls & ~(p3|p4), walls & ~(p1|p2|p3|p4)))
     else:
-        player = cp.where(((5 < positions) | (positions == 0)) ,positions,0)
-        wall = cp.array(((walls * cp.where(positions < 5,1,0)), walls * cp.where(positions == 0,1,0)))
-    
-    moves = cp.zeros_like(walls)
+        player = (p3 * 2) + (p4 * 4)
+        wall = cp.array((walls & ~(p1|p2), walls & ~(p1|p2|p3|p4)))
 
-    for i in cp.arange(len(directions)):
+    moves = cp.zeros(cp.shape(walls),cp.int8)
+
+    for i in cp.arange(8):
         pos = player.copy()
-        block = wall * edges[i]
+        block = wall & edges[i]
 
         for _ in cp.arange(3):
             pos = cp.roll(pos,directions[i])
             moves += pos * block[0]
             pos *= block[1]
+    return cp.array((wall[1],moves & 2, moves & 4),cp.bool_)
 
-    if p1Turn:
-        return cp.array((wall[1],moves & 2, moves & 4))
-    else:
-        return cp.array((wall[1],moves & 8, moves & 16))
-
-walls = cp.array(((0,1,1,1),(1,1,1,1),(1,0,1,1),(1,1,1,1)))
-positions = cp.array(((0,8,16,0),(0,0,0,0),(0,0,0,0),(2,0,0,4)))
-pieces = cp.array((((0,1),(0,2))((3,0),(3,3))))
+walls = cp.array(((False,True,True,True),(True,True,True,True),(True,False,True,True),(True,True,True,True)))
+p1 = cp.array(((False,False,False,False),(False,False,False,False),(False,False,False,False),(True,False,False,False)))
+p2 = cp.array(((False,False,False,False),(False,False,False,False),(False,False,False,False),(False,False,False,True)))
+p3 = cp.array(((False,True,False,False),(False,False,False,False),(False,False,False,False),(False,False,False,False)))
+p4 = cp.array(((False,False,True,False),(False,False,False,False),(False,False,False,False),(False,False,False,False)))
+pieces = cp.array((((0,1),(0,2)),((3,0),(3,3))))
 
 walls = cp.array((walls,walls))
-positions = cp.array((positions,positions))
+p1 = cp.array((p1,p1))
+p2 = cp.array((p2,p2))
+p3 = cp.array((p3,p3))
+p4 = cp.array((p4,p4))
 pieces = cp.array((pieces,pieces))
 
-moves = getMoves(walls,positions, True)
+moves = getMoves(walls,p1,p2,p3,p4, True)
 print (moves)
