13337b946fb1acc0dc76d96e20c0b87ea98c08d0
enigmaDevelopments <enigmarezac@gmail.com>
2025-02-04 11:50:28 -0500

    turned movement map maker into a function

--- cubot.py
+++ cubot.py
@@ -15,22 +15,27 @@ directions = cp.array((-5,-4,-3,-1,1,3,4,5))
 
 p = cp.array(((0,8,16,0),(0,0,0,0),(0,0,0,0),(2,0,0,4)))
 w = cp.array(((0,1,1,1),(1,1,1,1),(1,0,1,1),(1,1,1,1)))
-positions = cp.array((p,p))
-player = cp.where(positions < 5 ,positions,0)
-walls = cp.array(((cp.array((w,w)) * cp.where(((5 < positions) | (positions == 0)),1,0)), cp.array((w,w)) * cp.where(positions == 0,1,0)))
-moves = cp.zeros_like(positions)
 
-for i in cp.arange(len(directions)):
-    pos = player.copy()
-    block = walls * edges[i]
+def getMoves(wall: cp.ndarray, positions: cp.ndarray):
+    global edges
+    global directions
 
-    for j in cp.arange(3):
-        pos = cp.roll(pos,directions[i])
-        moves += pos * block[0]
-        pos *= block[1]
-p1Moves = moves & 2
-p2Moves = moves & 4
+    player = cp.where(positions < 5 ,positions,0)
+    walls = cp.array(((wall * cp.where(((5 < positions) | (positions == 0)),1,0)), wall * cp.where(positions == 0,1,0)))
+    moves = cp.zeros_like(positions)
 
-print(walls[1])
-print(p1Moves)
-print(p2Moves)
+    for i in cp.arange(len(directions)):
+        pos = player.copy()
+        block = walls * edges[i]
+
+        for _ in cp.arange(3):
+            pos = cp.roll(pos,directions[i])
+            moves += pos * block[0]
+            pos *= block[1]
+    return cp.array((walls[1],moves & 2, moves & 4))
+
+w = cp.array(((0,1,1,1),(1,1,1,1),(1,0,1,1),(1,1,1,1)))
+p = cp.array(((0,8,16,0),(0,0,0,0),(0,0,0,0),(2,0,0,4)))
+
+moves = getMoves(cp.array((w,w)),cp.array((p,p)))
+print (moves)
