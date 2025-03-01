d526c7849777fc8f7b1b23bef8faee3629208bfe
enigmaDevelopments <enigmarezac@gmail.com>
2025-03-01 11:02:46 -0500

    added a bot with a single thread

--- multibot.py
+++ multibot.py
@@ -1,3 +1,4 @@
+from concurrent import futures
 from gameState import gameState
 from random import choice
 from multiprocessing import freeze_support
@@ -36,17 +37,23 @@ def minMax(state : gameState, depth, alpha, beta):
             break
     return best
   
+def thread(info):
+  data = (minMax(info[0],1,-2000000,2000000), info[1])
+  return data
+  
 def ai(state):
   best = -2000000
   bestMove = None
-  for move in state.vaildMoves():
-    value = minMax(gameState(state.move(move)),1,best,2000000)
-    if value > best:
-      best = value
-      bestMove = [move]
-    elif value == best:
-      bestMove.append(move)
-
+  games = ((gameState(state.move(move)),move) for move in state.vaildMoves())
+  with futures.ProcessPoolExecutor() as executer:
+    values = [executer.submit(thread, node) for node in games]
+    for future in futures.as_completed(values):
+      value = future.result()
+      if value[0] > best:
+        best = value[0]
+        bestMove = [value[1]]
+      elif value[0] == best:
+        bestMove.append(value[1])
   return choice(bestMove) 
 
 if __name__ == "__main__":
