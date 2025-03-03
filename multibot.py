d86e1ac06f218b5bbe91577d75f64a1dda46a7a7
enigmaDevelopments <enigmarezac@gmail.com>
2025-03-03 10:22:53 -0500

    removed reused code

--- multibot.py
+++ multibot.py
@@ -2,40 +2,7 @@ from concurrent import futures
 from gameState import gameState
 from random import choice
 from multiprocessing import freeze_support
-
-
-def minMax(state : gameState, depth, alpha, beta):
-  end = state.endingBoard()
-  if end == 0:
-    return 1/(depth+2)
-  if end != None:
-    return end + ((depth*1000) + state.score() if end < 0 else -depth)
-  if depth == 7:
-    return state.score()
-  if state.p1Turn:
-    best = 2000000
-    for node in state.vaildMoves():
-      nextState = gameState(state.move(node))
-      value = minMax(nextState,depth+1,alpha,beta)
-      if value < best:
-        best = value
-        if best < beta:
-          beta = best
-          if beta <= alpha:
-            break
-    return best
-  else:
-    best = -2000000
-    for node in state.vaildMoves():
-      nextState = gameState(state.move(node))
-      value = minMax(nextState,depth+1,alpha,beta)
-      if value > best:
-        best = value
-        if best > alpha:
-          alpha = best
-          if beta <= alpha:
-            break
-    return best
+from bot import minMax
   
 def thread(info):
   data = (minMax(info[0],1,-2000000,2000000), info[1])
